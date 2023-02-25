2fe87b3d396529af732eb380be5ee29cf53920f3
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-02-25 01:52:53 -0300

    update

--- django_project/site_/core/views.py
+++ django_project/site_/core/views.py
@@ -9,18 +9,19 @@ def index(request):
 
 def catalogo(request):
     return render(request,'catalogo.html')
-
 def solicitar_financiamento(request):
+    mensagem = None # Inicializa a mensagem como vazia
     if request.method == 'POST':
         car_model = request.POST['car_model']
         loan_amount = request.POST['loan_amount']
         start_date = request.POST['start_date']
         end_date = request.POST['end_date']
         
-        #calcular jurso aqui
-        
+        #calcular juros aqui
         interest_rate = 0.05 
+        # Cálculo dos juros omitido
         
+        # Criação do objeto Carro
         Carro.objects.create(
             car_model = car_model,
             loan_amount = loan_amount,
@@ -29,4 +30,8 @@ def solicitar_financiamento(request):
             interest_rate = interest_rate
         )
         
-    return render(request,'financiamento.html')
+        # Define a mensagem de sucesso
+        mensagem = 'O FINANCIAMENTO FOI SOLICITADO COM SUCESSO!'
+        
+    return render(request, 'financiamento.html', {'mensagem': mensagem})
+
