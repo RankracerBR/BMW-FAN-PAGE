447a59e564b3397bf2942bfb8b20623eebabc168
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-03-16 04:58:32 -0300

    update

--- django_project/site_/core/views.py
+++ django_project/site_/core/views.py
@@ -9,6 +9,7 @@ def index(request):
 
 def catalogo(request):
     return render(request,'catalogo.html')
+
 def solicitar_financiamento(request):
     mensagem = None # Inicializa a mensagem como vazia
     if request.method == 'POST':
@@ -16,21 +17,25 @@ def solicitar_financiamento(request):
         loan_amount = request.POST['loan_amount']
         start_date = request.POST['start_date']
         end_date = request.POST['end_date']
-        
-        #calcular juros aqui
-        interest_rate = 0.05 
-        # Cálculo dos juros omitido
-        
-        # Criação do objeto Carro
-        Carro.objects.create(
-            car_model = car_model,
-            loan_amount = loan_amount,
-            start_date = start_date,
-            end_date = end_date,
-            interest_rate = interest_rate
-        )
-        
-        # Define a mensagem de sucesso
-        mensagem = 'O FINANCIAMENTO FOI SOLICITADO COM SUCESSO!'
-        
+        if not car_model or not loan_amount or not start_date or not end_date:
+            mensagem = 'Todos os campos devem ser preenchidos'
+        else:
+            #calcular juros aqui
+            interest_rate = 0.05 
+            # Cálculo dos juros omitido
+            
+            # Criação do objeto Carro
+            
+            Carro.objects.create(
+                car_model = car_model,
+                loan_amount = loan_amount,
+                start_date = start_date,
+                end_date = end_date,
+                interest_rate = interest_rate
+            )
+
+            # Define a mensagem de sucesso
+            mensagem = 'O FINANCIAMENTO FOI SOLICITADO COM SUCESSO!'
+    
     return render(request, 'financiamento.html', {'mensagem': mensagem})
+
